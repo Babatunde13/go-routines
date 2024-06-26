3e685a9fe4bcd0c62d8bdceda4d7bde31d31e08c
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-06-26 11:04:55 +0100

    get all and delete

--- routine.go
+++ routine.go
@@ -15,6 +15,10 @@ type DB struct {
 	users []User
 }
 
+func (u User) String () string {
+	return fmt.Sprintf("ID: %d, Name: %s", u.ID, u.Name)
+}
+
 func initDB () DB {
 	users := []User{
 		{ ID: 1, Name: "Babatunde"},
@@ -40,6 +44,12 @@ func updateUserInternal (users *[]User, id int, name string, user chan User) {
 	u := (*users)[id-1]
 	u.Name = name
 	(*users)[u.ID-1] = u
+	user <- u
+}
+
+func deleteUserInternal (users *[]User, id int, user chan User) {
+	u := (*users)[id-1]
+	*users = append((*users)[:id-1], (*users)[id:]...)
 	user <- u
 }
 
@@ -77,14 +87,36 @@ func (db *DB) updateUser (id int, name string) (error, User) {
 	return nil, <- u
 }
 
+func (db *DB) deleteUser (id int) (error, User) {
+	if id >= len(db.users) || id < 1 {
+		err := errors.New("User not found")
+		return err, User{}
+	}
+	u := make(chan User)
+	go deleteUserInternal(&db.users, id, u)
+
+	return nil, <- u
+}
+
+
 func (db *DB) createUser (name string) User {
 	user := User{
-		ID: len(db.users)+1,
+		ID: db.users[len(db.users)-1].ID + 1,
 		Name: name,
 	}
 	data := make(chan User)
 	go createUserInternal(&db.users, user, data)
 
+	return <- data
+}
+
+func getUsersInternal (users []User, user chan *[]User) {
+	user <- &users
+}
+
+func (db DB) getUsers () *[]User {
+	data := make(chan *[]User)
+	go getUsersInternal(db.users, data)
 	return <- data
 }
 
@@ -98,4 +130,9 @@ func main () {
 	fmt.Println(db.getUserById(30))
 	db.updateUser(1, "Babalola")
 	fmt.Println(db.users)
+	db.deleteUser(2)
+	fmt.Println(db.users)
+	db.createUser("RandomUser")
+	fmt.Println(db.users)
+	fmt.Println(db.getUsers())
 }
